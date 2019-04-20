2556567db092b47e28ec5f1682a281b0016a6312
Zach Kost-Smith <zachkostsmith@gmail.com>
2019-04-19 21:40:22 -0500

    Add rotation of cannon and some minor fixes/tweaks

--- split-shot.lisp
+++ split-shot.lisp
@@ -15,10 +15,16 @@
 (defvar *origin* (vec2 0 0))
 
 (defvar *cannon-pos* (vec2 (floor *width* 2) 0))
+(defvar *cannon-rotation* 0.0)
+(defvar *cannon-turn-right* nil)
+(defvar *cannon-turn-left* nil)
 
 (defvar *shot-pos* *cannon-pos*)
 
-(defvar *initial-vel* 30)
+(defvar *turn-right* nil)
+(defvar *turn-left* nil)
+
+(defvar *initial-vel* 50)
 
 (defvar *shot-vel* (vec2 0 30))
 
@@ -33,17 +39,20 @@
   "Create a perpendicular vector by rotating 90 degrees clockwise."
   (vec2 (y vec) (- (x vec))))
 
-(defvar *turn-right* nil)
-(defvar *turn-left* nil)
-
 (defmethod act ((app split-shot))
   (let ((new-time (real-time-seconds)))
-    (cond (*turn-right*
-           (let ((perp (normalize (perp-vec *shot-vel*))))
-             (setf *shot-vel* (add (mult 1 perp) *shot-vel*))))
-          (*turn-left*
-           (let ((perp (normalize (perp-vec *shot-vel*))))
-             (setf *shot-vel* (add (mult -1 perp) *shot-vel*)))))
+
+    (when *turn-right*
+      (let ((perp (normalize (perp-vec *shot-vel*))))
+        (setf *shot-vel* (add (mult 1 perp) *shot-vel*))))
+
+    (when *turn-left*
+      (let ((perp (normalize (perp-vec *shot-vel*))))
+        (setf *shot-vel* (add (mult -1 perp) *shot-vel*))))
+
+    (when *cannon-turn-left* (incf *cannon-rotation* 0.1))
+    (when *cannon-turn-right* (decf *cannon-rotation* 0.1))
+
     (setf *shot-pos* (add *shot-pos* (mult (- new-time *last-time*) *shot-vel*)))
     (setf *last-time* new-time)))
 
@@ -83,7 +92,7 @@
          ;; pass
          )))
 
-(defun key-release (key-index)
+(defun release-split (key-index)
   (cond ((= key-index 0)
          (setf *turn-right* nil))
         ((= key-index 9)
@@ -95,10 +104,16 @@
 (defmethod post-initialize ((this split-shot))
   ;; Initialize world state
   (setf *last-time* (real-time-seconds))
+
   (setf *shot-pos* *cannon-pos*)
   (setf *shot-vel* (vec2 0 0))
+
   (setf *turn-right* nil)
   (setf *turn-left* nil)
+
+  (setf *cannon-rotation* 0.0)
+  (setf *cannon-turn-right* nil)
+  (setf *cannon-turn-left* nil)
 
   ;; Setup bindings
   (add-bindings
@@ -107,13 +122,30 @@
       ((:q :w :e :r :t :y :u :i :o :p) :pressed
        (handle-split (key-index key)))
       ((:q :w :e :r :t :y :u :i :o :p) :released
-       (key-release (key-index key)))
+       (release-split (key-index key)))
+
+      (:a :pressed (setf *cannon-turn-left* t))
+      (:a :released (setf *cannon-turn-left* nil))
+      (:d :pressed (setf *cannon-turn-right* t))
+      (:d :released (setf *cannon-turn-right* nil))
+
       (:space :pressed
-              (setf *shot-vel* (vec2 0 *initial-vel*)))))
+              (setf *shot-pos* *cannon-pos*)
+              (setf *shot-vel*
+                    (vec2 (* *initial-vel* (- (sin *cannon-rotation*)))
+                          (* *initial-vel* (cos *cannon-rotation*)))))))
 
 (defun draw-shot ()
-  (draw-rect *origin*
-             10 5 :fill-paint *white* :rounding 5.0))
+  (let ((w 10) (h 5))
+    (draw-rect (vec2 (/ w -2) (/ h -2))
+               w h :fill-paint *white* :rounding 5.0)))
+
+(defun draw-cannon ()
+  (bodge-canvas:with-retained-canvas
+    (let ((w 7) (h 30))
+      (draw-circle *origin* 15 :fill-paint *white*)
+      (rotate-canvas 0)
+      (draw-rect (vec2 (/ w -2) 0) w h :fill-paint *white*))))
 
 (defmethod draw ((this split-shot))
   (draw-rect *origin*
@@ -121,4 +153,8 @@
   (bodge-canvas:with-retained-canvas
     (translate-canvas (x *shot-pos*) (y *shot-pos*))
     (rotate-canvas (atan (y *shot-vel*) (x *shot-vel*)))
-    (draw-shot)))
+    (draw-shot))
+  (bodge-canvas:with-retained-canvas
+    (translate-canvas (x *cannon-pos*) (y *cannon-pos*))
+    (rotate-canvas *cannon-rotation*)
+    (draw-cannon)))
