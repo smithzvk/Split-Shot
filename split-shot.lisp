375347a3c45079ebd23f5818ad6ff4f97fbbf902
Zach Kost-Smith <zachkostsmith@gmail.com>
2019-04-19 19:24:18 -0500

    Steer via holding down the keys rather than press events

--- split-shot.lisp
+++ split-shot.lisp
@@ -28,41 +28,93 @@
 (defvar *last-time* (real-time-seconds)
   "Denotes timestamp of the last model frame.")
 
-(defmethod gamekit:act ((app split-shot))
-  (let ((new-time (real-time-seconds)))
-    (setf *shot-pos* (add *shot-pos* (mult (- new-time *last-time*) *shot-vel*)))
-    (setf *last-time* new-time)))
-
-(defmacro add-bindings ((key state &body body) &rest more-bindings)
-  "Setup bindings without as much boiler plate."
-  `(progn (bind-button ,key ,state (lambda () ,@body))
-          ,@(if more-bindings
-                `((add-bindings ,@more-bindings))
-                nil)))
-
 (defun perp-vec (vec)
   "Create a perpendicular vector by rotating 90 degrees clockwise."
   (vec2 (y vec) (- (x vec))))
+
+(defvar *turn-right* nil)
+(defvar *turn-left* nil)
+
+(defmethod act ((app split-shot))
+  (let ((new-time (real-time-seconds)))
+    (cond (*turn-right*
+           (let ((perp (normalize (perp-vec *shot-vel*))))
+             (setf *shot-vel* (add (mult 1 perp) *shot-vel*))))
+          (*turn-left*
+           (let ((perp (normalize (perp-vec *shot-vel*))))
+             (setf *shot-vel* (add (mult -1 perp) *shot-vel*)))))
+    (setf *shot-pos* (add *shot-pos* (mult (- new-time *last-time*) *shot-vel*)))
+    (setf *last-time* new-time)))
+
+(defun ensure-list (x)
+  (if (consp x)
+      x
+      (list x)))
+
+(defmacro add-bindings ((keys state &body body) &rest more-bindings)
+  "Setup bindings without as much boiler plate."
+  (let ((lambda-sym (gensym "ADD-BINDINGS-")))
+    `(progn (let ((,lambda-sym (lambda (key)
+                                 (declare (ignorable key))
+                                 ,@body)))
+              ,@(iter (for k :in (ensure-list keys))
+                  (collect `(bind-button
+                             ,k ,state
+                             (lambda () (funcall ,lambda-sym ,k))))))
+            ,@(if more-bindings
+                  `((add-bindings ,@more-bindings))
+                  nil))))
+
+(defun key-index (key)
+  (case key
+    (:q 0)
+    (:w 1)
+    (:e 2)
+    (:r 3)
+    (:t 4)
+    (:y 5)
+    (:u 6)
+    (:i 7)
+    (:o 8)
+    (:p 9)))
+
+(defun handle-split (key-index)
+  (cond ((= key-index 0)
+         (setf *turn-right* t))
+        ((= key-index 9)
+         (setf *turn-left* t))
+        (t
+         ;; pass
+         )))
+
+(defun key-release (key-index)
+  (cond ((= key-index 0)
+         (setf *turn-right* nil))
+        ((= key-index 9)
+         (setf *turn-left* nil))
+        (t
+         ;; pass
+         )))
 
 (defmethod post-initialize ((this split-shot))
   ;; Initialize world state
   (setf *last-time* (real-time-seconds))
   (setf *shot-pos* *cannon-pos*)
   (setf *shot-vel* (vec2 0 0))
+  (setf *turn-right* nil)
+  (setf *turn-left* nil)
 
   ;; Setup bindings
   (add-bindings
       (:escape :pressed
         (stop))
-      (:q :pressed
-          (let ((perp (normalize (perp-vec *shot-vel*))))
-            (setf *shot-vel* (add (mult 1 perp) *shot-vel*))))
-      (:p :pressed
-          (let ((perp (normalize (perp-vec *shot-vel*))))
-            (setf *shot-vel* (add (mult -1 perp) *shot-vel*))))
+      ((:q :w :e :r :t :y :u :i :o :p) :pressed
+       (handle-split (key-index key)))
+      ((:q :w :e :r :t :y :u :i :o :p) :released
+       (key-release (key-index key)))
       (:space :pressed
               (setf *shot-vel* (vec2 0 *initial-vel*)))))
 
 (defmethod draw ((this split-shot))
   (draw-rect (add *shot-pos* *origin*)
-             5 10 :fill-paint *black* :rounding 5.0))
+             10 10 :fill-paint *black* :rounding 5.0))
